6995e9819930ea89d93ae508699dc9b41eb10fdf
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-12-28 22:55:56 +0600

    Initial commit

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DBName } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_CONNECTION_URI}/${DBName}`)
+        console.log("MONGODB CONNECTED || DB HOST", connectionInstance.connection.host);
+    } catch (error) {
+        console.log("MONGODB CONNECTION FAILED", error);
+    }
+}
+
+export default connectDB
