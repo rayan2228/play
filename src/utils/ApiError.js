a9a45b6a3ab90c3ed297d5f2c01a45802789d2ed
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2024-01-04 21:41:50 +0600

    api error handle

---
+++ src/utils/ApiError.js
@@ -0,0 +1,17 @@
+
+
+class ApiError extends Error {
+    constructor(statusCode, message = "something went wrong", errors = [], stack = "") {
+        super(message)
+        this.statusCode = statusCode >= 400
+        this.errors = errors
+        this.message = message
+        this.data = []
+        this.success = false
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
